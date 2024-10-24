03c19476e04e57493a68d0cecbd88c08c8ab20e9
Ritik <ritikraw@gmail.com>
2024-10-24 13:39:41 +0530

    add footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,12 +1,62 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faLinkedin,
+  faYoutube,
+  IconDefinition,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  FontAwesomeIcon,
+  FontAwesomeIconProps,
+} from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Footer = () => {
+  const footerData = {
+    company: {
+      title: "Company",
+      links: [
+        { name: "Our Story", href: "#" },
+        { name: "Careers at AAJ", href: "#" },
+        { name: "Contact Us", href: "#" },
+      ],
+    },
+    services: {
+      title: "Services",
+      links: [
+        { name: "E Commerce Fulfillment", href: "#" },
+        { name: "Transport-AAJ Swift", href: "#" },
+        { name: "Returns Management", href: "#" },
+        { name: "Value Added Services", href: "#" },
+      ],
+    },
+    resources: {
+      title: "Resources",
+      links: [
+        { name: "Case Studies", href: "#" },
+        { name: "Blogs", href: "#" },
+      ],
+    },
+    support: {
+      title: "Support",
+      links: [{ name: "Track Your Order", href: "#" }],
+    },
+    transportation: {
+      title: "Transportation Network",
+      links: [
+        { name: "Transportation in Mumbai", href: "#" },
+        { name: "Transportation in Bangalore", href: "#" },
+        { name: "Transportation in Delhi", href: "#" },
+        { name: "Transportation in Hyderabad", href: "#" },
+        { name: "Transportation in Ghaziabad", href: "#" },
+        { name: "Transportation in Sonipat NCR", href: "#" },
+      ],
+    },
+  };
+
   return (
     <footer className="w-full">
-      <div>
-        <div>
-          {" "}
+      <div className="container mx-auto flex items-center justify-between pb-6 border-b border-[#C6BBBB]">
+        <div className="flex gap-16">
           <img
             src="/assests/AAJ-Logo-in-without-tagline-8-1-2.png"
             alt="logo"
@@ -17,13 +67,56 @@ const Footer = () => {
             Optimizing supply chains through <br />
             warehousing and transportation services
           </p>
-        </div>{" "}
-        <div>
-          <span>{/* <FontAwesomeIcon icon={faline} /> */}</span>
+        </div>
+        <div className="space-x-6">
+          <SocialButton icon={faLinkedin} />
+          <SocialButton icon={faInstagram} />
+          <SocialButton icon={faYoutube} />
+        </div>
+      </div>
+
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8">
+          {Object.entries(footerData).map(([key, section]) => (
+            <div key={key} className="space-y-5">
+              <h3 className="text-sm font-semibold text-gray-900 tracking-wider uppercase">
+                {section.title}
+              </h3>
+              <ul className="mt-4 space-y-3">
+                {section.links.map((link, index) => (
+                  <li key={index}>
+                    <a
+                      href={link.href}
+                      className="text-base text-gray-600 hover:text-blue-600"
+                    >
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div className="mt-8 border-t pt-8 flex justify-between">
+          <p className="text-center text-sm text-gray-500">
+            © 2024 AAJ Swift. All Rights Reserved
+          </p>
+          <p className="text-center text-sm text-gray-500">
+            ISO 9001: 2015, ISO 27001: 2013 Certified Company &nbsp; | &nbsp;
+            CIN: L63090DL2011PLC221234
+          </p>
         </div>
       </div>
     </footer>
   );
 };
+
+function SocialButton({ icon }: { icon: IconDefinition }) {
+  return (
+    <button className="btn btn-circle bg-transparent p-3 rounded-full border border-[#C6BBBB]">
+      <FontAwesomeIcon className="text-2xl" icon={icon} />
+    </button>
+  );
+}
 
 export default Footer;
